b7d164f4d931d904bd89eb5e28fa2e2d8c2034bf
nguyenthe <nguyenthe23031998@gmail.com>
2021-07-10 21:01:04 +0700

    modifiled MainActivity

--- app/src/main/java/com/example/github_2/MainActivity.kt
+++ app/src/main/java/com/example/github_2/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.github_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("ABC","dssgsg")
+        Log.d( "ABC","sfsfes" )
     }
 }
